0de84c6f05e679dc49195e5d51b5123e982d00d5
Thaysa Lacerda <t_thaysa.lacerda@hapvida.com.br>
2024-12-08 21:23:02 -0400

    feat: armazenando dados / validando cep com BrasilAPI - service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './user.model';
+import axios from 'axios';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users: User[] = [];
+
+  createUser(user: User): User {
+    // user.id = this.users.length + 1; //ainda não sei a necessidade de inserir id
+    this.users.push(user);
+    return user;
+  }
+
+  getAllUsers(): User[] {
+    return this.users;
+  }
+
+  // Quando acresecntar futuramente para facilitar busca do usuário
+  // getUserById(id: number): User {
+  //   return this.users.find((user) => user.id === id);
+  // }
+
+  // updateUser(id: number, updatedUser: Partial<User>): User {
+  //   const userIndex = this.users.findIndex((user) => user.id === id);
+  //   if (userIndex === -1) {
+  //     throw new Error('User not found');
+  //   }
+  //   this.users[userIndex] = { ...this.users[userIndex], ...updatedUser };
+  //   return this.users[userIndex];
+  // }
+
+  async validateCep(cep: string): Promise<boolean> {
+    try {
+      const response = await axios.get(
+        `https://brasilapi.com.br/api/cep/v1/${cep}`,
+      );
+      return response.status === 200;
+    } catch {
+      return false;
+    }
+  }
+}
