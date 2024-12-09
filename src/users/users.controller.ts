edd12f3911a33b5d86bba50651c60c737bb4fea2
Thaysa Lacerda <t_thaysa.lacerda@hapvida.com.br>
2024-12-08 21:23:19 -0400

    feat: criando as rotas - controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Get, Body } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User } from './user.model';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  async createUser(@Body() user: User) {
+    const isValidCep = await this.usersService.validateCep(user.cep);
+    if (!isValidCep) {
+      return { error: 'CEP inválido' };
+    }
+    return this.usersService.createUser(user);
+  }
+
+  @Get()
+  getAllUsers() {
+    return this.usersService.getAllUsers();
+  }
+
+  //   @Put(':id')
+  //   updateUser(@Param('id') id: string, @Body() updatedUser: Partial<User>) {
+  //     return this.usersService.updateUser(+id, updatedUser);
+  //   }
+}
